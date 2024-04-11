16b858ad1a9707519f36860c1ab0ebc8fc0ed498
Александр Балезин <a.balezin@mail.ru>
2024-04-11 11:04:51 +0300

    ninteenth commit

--- backend/foodgram/views.py
+++ backend/foodgram/views.py
@@ -108,7 +108,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     http_method_names = ('get', 'post', 'patch', 'delete',
                          'head', 'options', 'trace')
     queryset = Recipe.objects.all()
-    serializer_class = RecipeSerializer
+    #serializer_class = RecipeSerializer
     pagination_class = CustomPagination
     permission_classes = (IsAuthorOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
