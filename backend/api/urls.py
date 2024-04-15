5c960accf5bea0f5fa6a69d00f03c3a6fbaaf7cb
Александр Балезин <a.balezin@mail.ru>
2024-04-15 17:18:18 +0300

    7th review2 fix

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -12,6 +12,6 @@ router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('djoser.urls.authtoken')),
-    path('api/', include(router.urls)),
+    path('/auth/', include('djoser.urls.authtoken')),
+    path('/', include(router.urls)),
 ]
