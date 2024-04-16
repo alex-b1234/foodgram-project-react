43fcce4ce80046b4fa71f6ecbcb2cbd6871d7187
Александр Балезин <a.balezin@mail.ru>
2024-04-16 16:11:45 +0300

    30th review2 fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -171,9 +171,11 @@ class RecipeViewSet(viewsets.ModelViewSet):
             )
         )'''
         cart_ingredients = (
-            RecipeIngredient.objects.values(
+            RecipeIngredient.objects.filter(
+                recipe__cart__user=request.user
+            ).values(
                 'ingredient__name',
-            #    'ingredient__measurement_unit'
+                'ingredient__measurement_unit'
             ).annotate(cart_amount=Sum('amount'))#.order_by('-amount')
         )
         shopping_list = ''
@@ -181,7 +183,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         for item in cart_ingredients:
             num += 1
             name = item['ingredient__name']
-        #    measurement_unit = item['ingredient__measurement_unit']
+            measurement_unit = item['ingredient__measurement_unit']
             measurement_unit = 'г'
             amount = item['cart_amount']
             shopping_list += (f'{num}. {name} - '
