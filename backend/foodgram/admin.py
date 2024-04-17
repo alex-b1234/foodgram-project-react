23246708acbe53548281c3b41627898952fbe948
Александр Балезин <a.balezin@mail.ru>
2024-04-18 00:45:17 +0300

    review4 fix

--- backend/foodgram/admin.py
+++ backend/foodgram/admin.py
@@ -3,9 +3,23 @@ from django.contrib import admin
 from .models import (Tag, Recipe, Ingredient, Cart, Favorite, Follow,
                      RecipeIngredient, RecipeTag)
 
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author')
+    list_filter = ('author', 'name', 'tags')
+
+    def favorite_count(self, obj):
+        return Favorite.objects.filter(recipe=obj).count()
+
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name',)
+
+
 admin.site.register(Tag)
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Favorite)
 admin.site.register(Cart)
 admin.site.register(Follow)
